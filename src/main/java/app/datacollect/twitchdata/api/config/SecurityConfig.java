87e0e6ab91f9dbabf0a05c17f0fce707cb564d90
DataCollectApp <datacollectapp@jarand.dev>
2020-01-05 10:25:42 +0100

    #9: Ignore CSRF for search endpoint

--- src/main/java/app/datacollect/twitchdata/api/config/SecurityConfig.java
+++ src/main/java/app/datacollect/twitchdata/api/config/SecurityConfig.java
@@ -14,6 +14,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     super.configure(http);
+
+    http.csrf().ignoringAntMatchers("/twitch-user/search/**");
   }
 
   @Override

6773c82bbf7ded081ea7af3c46bdbbf8a6a1d095
DataCollectApp <datacollectapp@jarand.dev>
2020-01-05 10:32:36 +0100

    #9: Added JsonCreator annotation

--- src/main/java/app/datacollect/twitchdata/api/twitchuser/resource/TwitchUserSearch.java
+++ src/main/java/app/datacollect/twitchdata/api/twitchuser/resource/TwitchUserSearch.java
@@ -1,10 +1,12 @@
 package app.datacollect.twitchdata.api.twitchuser.resource;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.List;
 
 public class TwitchUserSearch {
   private final List<Long> userIds;
 
+  @JsonCreator
   public TwitchUserSearch(List<Long> userIds) {
     this.userIds = userIds;
   }
